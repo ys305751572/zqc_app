0158bb4ff37289f65cb2aa45fec113ea5ab80ad9
ys305751572 <305751572@qq.com>
2016-07-28 17:56:36 +0800

    商品

---
+++ src/main/java/com/leoman/product/service/ProductService.java
@@ -0,0 +1,16 @@
+package com.leoman.product.service;
+
+
+import com.leoman.common.service.GenericManager;
+import com.leoman.product.entity.Product;
+import com.leoman.task.entity.Task;
+import org.springframework.data.domain.Page;
+
+/**
+ * Created by Daisy on 2016/7/25.
+ */
+public interface ProductService extends GenericManager<Product> {
+
+    public Page<Product> findAll(Integer currentPage, Integer pageSize);
+
+}
